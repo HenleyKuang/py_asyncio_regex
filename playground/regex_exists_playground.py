9c071d67c8c973a80f3afd004da5e9439f228657
Henley Kuang <hkuang@brightedge.com>
2020-07-12 00:15:48 -0700

    Add wrapper function in regex exists playground to add async sleep and logging to understand what's going on with async tasks

--- playground/regex_exists_playground.py
+++ playground/regex_exists_playground.py
@@ -1,10 +1,17 @@
 import argparse
 import asyncio
 import logging
+import random
 
 from py_asyncio_regex.regex_exists import regex_exists
 
 LOGGER = logging.getLogger(__name__)
+
+
+async def regex_exists_with_sleep(task_id, regex_pattern, string_value):
+    random_int = random.randint(1, 10)
+    await asyncio.sleep(random_int)
+    return await regex_exists(regex_pattern, string_value), task_id, random_int
 
 
 def _parse_args():
@@ -20,15 +27,26 @@ async def _main():
     regex_pattern = args.regex_pattern
     string_values = args.string_values
 
+    # Create async tasks
     task_list = []
+    task_id = 0
     for string_value in string_values:
+        task_id += 1
         task = asyncio.create_task(
-            regex_exists(regex_pattern, string_value)
+            regex_exists_with_sleep(task_id, regex_pattern, string_value)
         )
         task_list.append(task)
+    # Check how many tasks remaining
+    active_task_count = len(asyncio.all_tasks()) - 1
+    while active_task_count > 0:
+        LOGGER.info("Active Tasks: %s", active_task_count)
+        await asyncio.sleep(1)
+        active_task_count = len(asyncio.all_tasks()) - 1
+    # When all tasks complete, print the results
     for t in task_list:
-        r = await t
-        LOGGER.info(r)
+        r, task_id, random_int = await t
+        LOGGER.info("Task Id: %s, Result: %s, Random Sleep: %s",
+                    task_id, r, random_int)
 
 
 if __name__ == "__main__":
